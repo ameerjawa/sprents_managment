fbc2d1da52106d089cd96fa08d65e2a91d06dc02
ameerjawa <ameer0233@gmail.com>
2022-07-12 17:40:16 +0300

    first initial

--- utils/routes.js
+++ utils/routes.js
@@ -17,7 +17,7 @@ function routes(app) {
   
     // All other GET requests not handled before will return our React app
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '../training/build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
     });
 
 	// app.get('/redirect/*', SC.guest.get.redirect);
